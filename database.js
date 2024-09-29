3971984bd6ed90cf0ff4c34c54bdcf6ab89bdbbf
sampdevi <dev86.sean@gmail.com>
2024-09-29 17:54:02 +0800

    revised console message

--- database.js
+++ database.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.DB_URL)
   .then(() => {
-    console.log("[GALZify] Connected to MongoDB Database successful.");
+    console.log("[LSCSlinks] Connected to MongoDB Database successful.");
   })
   .catch((e) => {
-    console.error(`[GALZify] Connection to MongoDB Database has failed. Error: ${e}`);
+    console.error(`[LSCSlinks] Connection to MongoDB Database has failed. Error: ${e}`);
   })
 
 module.exports = mongoose
