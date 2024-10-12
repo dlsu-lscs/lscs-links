0273e67cfe218516564a95e83c26012dfe07e7d1
sampdevi <dev86.sean@gmail.com>
2024-10-12 17:38:40 +0800

    feat(reset password): init

--- controllers/user.js
+++ controllers/user.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const userModel = require('../models/user');
 const userAuthMiddleware = require('../middleware/auth');
 const userAuth = userAuthMiddleware;
+const mailer = require('nodemailer');
 
 // Create User
 
@@ -118,5 +119,68 @@ router.delete('/:id', async (req, res) => {
     res.status(500).send({ status: 'error', error: error.message });
   }
 });
+
+router.post('/reset-password', async (req, res) => {
+  try {
+    const updates = req.body;
+    if (updates.password) {
+      updates.password = await bcrypt.hash(updates.password, 10);
+    }
+    const user = await userModel.findByIdAndUpdate(req.params.id, updates, { new: true });
+    if (!user) {
+      return res.status(404).send({ status: 'error', error: 'User not found.' });
+    }
+    res.status(200).send({ status: 'success', user });
+  } catch (error) {
+    res.status(500).send({ status: 'error', error: error.message });
+  }
+})
+
+router.get('/request-reset', async (req, res) => {
+  try {
+    const user = await userModel.findOne({ email }).exec();
+    if (!user) {
+      console.log('invalid user');
+      return;
+    }
+    const email = req.query.email;
+
+    const transporter = mailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.APP_EMAIL,
+        pass: process.env.APP_PASSWORD
+      }
+    });
+
+    // RESET_PASSWORD_TOKEN
+    const token = jwt.sign(
+      { userId: user._id, email: user.email }, // Payload
+      process.env.JWT_SECRET,                  // Secret key (set in environment)
+      { expiresIn: '1h' }                      // Token expiration time
+    );
+
+    const mailOptions = {
+      from: 'lscs@dlsu.edu.ph',
+      to: email,
+      subject: 'hello',
+      text: `your reset token is this ${token}`
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).send({ message: 'Error sending email', error });
+      } else {
+        console.log('Email sent: ' + info.response);
+        return res.status(200).send({ message: 'Email sent successfully!' });
+      }
+    });
+  }
+  catch (err) {
+    console.log('hello fail :(')
+    throw err
+  }
+})
 
 module.exports = router;
