311f83b8b812638536ac6bbeec73eb11ea320d59
sampdevi <dev86.sean@gmail.com>
2024-10-13 21:20:09 +0800

    dettached reset password feature from dev branch

--- controllers/user.js
+++ controllers/user.js
@@ -5,7 +5,6 @@ const router = express.Router();
 const userModel = require('../models/user');
 const userAuthMiddleware = require('../middleware/auth');
 const userAuth = userAuthMiddleware;
-const mailer = require('nodemailer');
 
 // Create User
 
@@ -119,78 +118,5 @@ router.delete('/:id', async (req, res) => {
     res.status(500).send({ status: 'error', error: error.message });
   }
 });
-
-router.post('/reset-password', async (req, res) => {
-  try {
-    const updates = req.body;
-
-    if (updates.password == null || updates.password == undefined) {
-      return res.send({ status: 'error', message: 'No new password given.' });
-    }
-    else {
-      updates.password = await bcrypt.hash(updates.password, 10);
-    }
-    jwt.verify(token, process.env.JWT_SECRET, async (err, user) => {
-      if (err) return res.status(403).json({ status: 'error', message: 'Invalid token, access denied' });
-
-      const user_to_update = await userModel.findAndUpdate(user.email, { password: updates.password }, { new: true });
-      if (!user_to_update) {
-        return res.status(404).send({ status: 'error', error: 'User not found.' });
-      }
-      res.status(200).send({ status: 'success', message: 'Password changed successfully' });
-
-    });
-  } catch (error) {
-    res.status(500).send({ status: 'error', error: error.message });
-  }
-})
-
-router.get('/request-reset', async (req, res) => {
-  try {
-    const email = req.params.email;
-    const user = await userModel.findOne({ email: email }).exec();
-    console.log(user)
-    if (!user) {
-      console.log('invalid user');
-      return;
-    }
-
-    const transporter = mailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.APP_EMAIL,
-        pass: process.env.APP_PASSWORD
-      }
-    });
-
-    // RESET_PASSWORD_TOKEN
-    const token = jwt.sign(
-      { userId: user._id, email: user.email }, // Payload
-      process.env.JWT_SECRET,                  // Secret key (set in environment)
-      { expiresIn: '1h' }                      // Token expiration time
-    );
-
-    const mailOptions = {
-      from: 'lscs@dlsu.edu.ph',
-      to: email,
-      subject: 'hello',
-      text: `your reset token is this ${token}`
-    }
-
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error);
-        return res.status(500).send({ message: 'Error sending email', error });
-      } else {
-        console.log('Email sent: ' + info.response);
-        return res.status(200).send({ message: 'Email sent successfully!' });
-      }
-    });
-  }
-  catch (err) {
-    console.log('hello fail :(')
-    throw err
-  }
-})
 
 module.exports = router;
