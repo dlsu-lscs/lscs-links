16d06f94c36fb601586c58b84cce32a1bd61b97f
zelkim <dev86.sean@gmail.com>
2024-12-04 14:13:38 +0800

    printed token debug

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,9 +11,10 @@ const auth = async (req, res, next) => {
   }
 
   try {
+    console.log(token)
     const ticket = await client.verifyIdToken({
       idToken: token,
-      audience: process.env.CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      audience: process.env.GOOGLE_CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
       // Or, if multiple clients access the backend:
       //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
     });
